1bf9072731a9b84e0a00ca72ebe4b46654a19717
VirTEK11 <jsnanodegree@gmail.com>
2019-01-14 18:23:44 +0500

    Async Image Fetching

--- app.js
+++ app.js
@@ -37,3 +37,16 @@ async function PointSearche(){
 const result=PointSearche()
 //result all image со значения user ссылка из даты catid используя Quakka.js 
 //открыл xthtp ctrl + shift + p
+
+async function PointAsyncSearche()
+const response =await fetch()
+const user =await response.json()
+const catImageUrls =[]
+for (const catId of user.cats){
+    const response =await fetch()
+    const catData = await response.json()
+    catImageUrls.push(catData.imageUrl)
+}
+return catImageUrls
+const result=PointAsyncSearche()
+result
