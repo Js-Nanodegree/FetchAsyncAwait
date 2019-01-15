7585ce7d55868dba4e324e20457ab876daa7f86f
VirTEK11 <jsnanodegree@gmail.com>
2019-01-15 12:36:41 +0500

    allGood

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan')
+const bodyparser =require('body-parser')
+
 
 const productRoutes = require('./api/routes/product');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
 app.use(morgan('dev'))
-
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
-
+app.use((req,res,next) =>{
+    const error =new Error('Not Found')
+    error.status(404)
+    next(error)
+})
 app.use((error,req,res,next) =>{
     res.status(error.status || 500)
     res.json({
