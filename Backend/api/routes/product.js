55104aca968dbed3a159c7af5a6962d0f7b83ec4
VirTEK11 <jsnanodegree@gmail.com>
2019-01-18 14:47:58 +0500

    websocket mongo encrypted

---
+++ Backend/api/routes/product.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose')
+
+const Product = require('../models/product')
+
+
+
+router.post("/", (req, res, next) => {
+    const product = new Product({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      });
+    product
+      .save()
+      .then(result => {
+        console.log(result);
+        res.status(201).json({
+          message: "Handling POST requests to /products",
+          createdProduct: result
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
+    });
+});
+
+module.exports = router; 
